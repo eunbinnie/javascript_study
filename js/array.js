0c7f2cb2fa63aee1b6a80c60819afe8885d9493d
eunbinnie <dmsqlsdl54@naver.com>
2022-12-20 11:58:33 +0900

    array basic

--- js/array.js
+++ js/array.js
@@ -30,3 +30,57 @@ for (let fruit of fruits) {
 
 // c.forEach
 fruits.forEach((fruit) => console.log(fruit));
+
+
+// 4. Addition, deletion, copy
+// push: add an item to the end
+fruits.push('strawberry', 'orange');
+console.log(fruits);
+
+// pop: remove an item from the end
+fruits.pop();
+fruits.pop();
+console.log(fruits);
+
+// unshift: add an item to the beginning
+fruits.unshift('lemon', 'melon');
+console.log(fruits);
+
+// shift: remove an item from the beginning
+fruits.shift();
+fruits.shift();
+console.log(fruits);
+
+// note! shift, unshift are slower than pop, push
+// splice: remove an item by index position
+fruits.push('lemon', 'melon', 'orange');
+console.log(fruits);
+fruits.splice(1, 1);
+console.log(fruits);
+fruits.splice(1, 1, 'watermelon', 'cherry');
+console.log(fruits);
+
+// combine two arrays
+const fruits2 = ['strawberry', 'blueberry'];
+const newFruits = fruits.concat(fruits2);
+console.log(newFruits);
+
+
+// Searching
+// indexOf: find the index
+console.clear();
+console.log(fruits);
+console.log(fruits.indexOf('apple'));
+console.log(fruits.indexOf('cherry'));
+console.log(fruits.indexOf('blueberry'));
+
+// includes
+console.log(fruits.includes('melon'));
+console.log(fruits.includes('blueberry'));
+
+// lastIndexOf
+console.clear();
+fruits.push('cherry');
+console.log(fruits);
+console.log(fruits.indexOf('cherry'));
+console.log(fruits.lastIndexOf('cherry'));
